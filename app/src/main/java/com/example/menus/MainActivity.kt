fe077274fabb2571bc968f8860263ad9ba7bf564
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-02-23 17:21:25 +0300

    Contextual Menu Implementation

--- app/src/main/java/com/example/menus/MainActivity.kt
+++ app/src/main/java/com/example/menus/MainActivity.kt
@@ -3,23 +3,34 @@ package com.example.menus
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
+import android.os.Handler
+import android.os.PersistableBundle
+import android.view.*
 import android.widget.ImageSwitcher
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var image:ImageView
+    private lateinit var text : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        image = findViewById(R.id.imageView)
 
+        //splashscreen
+       /* val intent = Intent(this@MainActivity,SplashScreen::class.java)
+        startActivity(intent)
+        finish()*/
+
+        image = findViewById(R.id.imageView)
+        text = findViewById(R.id.textView2)
+        //registering textview for context menu
+        registerForContextMenu(text)
         image.setOnClickListener { openGallery() }
     }
 
@@ -38,17 +49,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId==R.id.option1){
-            Toast.makeText(this,"Option1",Toast.LENGTH_SHORT).show()
+        if (item.itemId==R.id.option1){
+            Toast.makeText(this,"Option1 Selected",Toast.LENGTH_SHORT).show()
         }
         else if(item.itemId==R.id.option2){
-            Toast.makeText(this,"Option2",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"Option2 Selected",Toast.LENGTH_SHORT).show()
         }
         else if(item.itemId==R.id.option3){
-            Toast.makeText(this,"Option3",Toast.LENGTH_SHORT).show()
+            Toast.makeText(this,"Option3 Selected",Toast.LENGTH_SHORT).show()
         }
         else
             Toast.makeText(this,"No Option Selected",Toast.LENGTH_SHORT).show()
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        val menuInflater : MenuInflater = menuInflater
+        menuInflater.inflate(R.menu.floating_context_menu,menu)
+        super.onCreateContextMenu(menu, v, menuInfo)
     }
 }
